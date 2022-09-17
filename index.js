2a072c06314e30c164fc0e52b14365ce86fff78e
brychxpin <bryan.oliveros17@tectijuana.edu.mx>
2022-09-16 22:20:02 -0700

    Update Bpm Function

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
 const compass = document.getElementById('compass'); // get compass element
 const children = document.getElementsByClassName('beat'); //get array of children
-const focus = document.getElementsByClassName('beatFocus'); 
+const focus = document.getElementsByClassName('beatFocus');
 
+
+const bar = Array.from(children); // convert to array
 const seg = 1000;
 
-// get current time
-const changeColor = () => {
-    let beat = 0;
-    setInterval(function(){
-        children[0].classList.toggle('beatFocus') // toggle class
-        beat++;
-        if(beat === children.length){
-            beat = 0;
-        }
-    }, seg);
-};
-changeColor();
 
-// const remove = () => {
-//     let beat = 0;
-//     setInterval(function(){
-//         children[beat].classList.remove('beatFocus')
-//         beat++;
-//         if(beat === children.length){
-//             beat = 0;
-//         }
-//     }, 1000);
-// };
+const bpm = (bpm) => {
+    return (seg / bpm) * 60;
+}
 
-// let remove = () => {
-//     for (let i = 0; i < children.length; i++) {
-//         children[i].classList.remove('beatFocus');
-//     }
+const metronome = () => { 
+    setInterval(() => {
+        bar.forEach((e) => {
+            e.classList.remove('beatFocus');
+        });
+        bar[0].classList.add('beatFocus');
+        bar.push(bar.shift());
+    }, bpm(90));
+}
+
+metronome();
+
 
 console.log(children);
+console.log(bar);
 
 
